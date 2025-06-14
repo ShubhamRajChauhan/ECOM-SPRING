719df2d522ad766a838a8f5f699dd54d4f070e71
Shubham <shubhamrajchauhan07@gmail.com>
2025-06-14 14:28:23 +0530

    CORS

--- ecom-project/src/main/java/com/telusko/ecom_project/controller/ProductController.java
+++ ecom-project/src/main/java/com/telusko/ecom_project/controller/ProductController.java
@@ -4,13 +4,14 @@ package com.telusko.ecom_project.controller;
 import com.telusko.ecom_project.model.Product;
 import com.telusko.ecom_project.service.ProdductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class ProductController {
 
@@ -18,13 +19,19 @@ public class ProductController {
     private ProdductService service;
 
 
-    @RequestMapping("/")
-    public String greet() {
-        return "Hello World";
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
     }
 
-    @GetMapping("/products")
-    public List<Product> getAllProducts() {
-        return service.getAllProducts();
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {
+
+        Product product = service.getProductById(id);
+
+        if(product != null)
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- ecom-project/src/main/java/com/telusko/ecom_project/model/Product.java
+++ ecom-project/src/main/java/com/telusko/ecom_project/model/Product.java
@@ -35,6 +35,7 @@
 
 package com.telusko.ecom_project.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -54,6 +55,8 @@ public class Product {
     private String brand;
     private BigDecimal price;
     private String category;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date releaseDate;
     private boolean available;
     private int quantity;
