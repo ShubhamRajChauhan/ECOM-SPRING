4c7e92989c3d92ceb1b5353954ebea4113b0e9d7
Shubham <shubhamrajchauhan07@gmail.com>
2025-06-14 10:07:58 +0530

    Revert "frontend part"
    This reverts commit a66c165968bef2175dea85bb59fe319d2d000af8.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,8 +25,8 @@ const Navbar = ({ onSelectCategory, onSearch }) => {
       <header>
         <nav className="navbar navbar-expand-lg fixed-top">
           <div className="container-fluid">
-            <a className="navbar-brand" href="https://github.com/ShubhamRajChauhan/ECOM-SPRING">
-              ECOM-SPRING
+            <a className="navbar-brand" href="https://telusko.com/">
+              Telusko
             </a>
             <button
               className="navbar-toggler"
